ef8220137aa7841c41bf84eeaf3ffa3aae1998c4
mohitthakral93 <mohit.thakral93@gmail.com>
2017-11-16 10:31:08 +0530

    adding code to featured js

--- react-router/src/js/components/pages/featured.js
+++ react-router/src/js/components/pages/featured.js
@@ -3,8 +3,10 @@ import React from "react";
 export default class Featured extends React.Component{
   render(){
     return(
-      <h1>Featured</h1>
-      <h3>Hello</h3>
+      <div>
+        <h1>Featured</h1>
+        <h3>Hello</h3>
+      </div>
     );
   }
 }
